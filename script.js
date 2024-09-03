661b590329734a53575186b8d2c82041ab75309d
marryjane077 <analiviafrancorinaldi@gmail.com>
2024-09-03 16:35:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const  avanca  =  document . querySelectorAll ('.btn-proximo');
+const  avanca = document.querySelectorAll ('.btn-proximo');
 
-avanca . forEach ( botão  =>  {
-    botão . addEventListener ('clique' ,  função ( ){
-        const  atual =  documento . querySelector ('.ativo' );
-        const  proximoPasso =  'passo-'  +  this.getAttribute ('data-proximo');
+avanca.forEach(button=>  {
+    button.addEventListener('click', function(){
+        const  atual = document.querySelector('.ativo');
+        const  proximoPasso = 'passo-'  +  this.getAttribute ('data-proximo');
 
-        atual.lista de classes.remover( 'ativo' );
-        documento.getElementById ( proximoPasso ).classList.add('ativo');
-    } )
-} )
+        atual.classList.remove('ativo');
+        document.getElementById (proximoPasso).classList.add('ativo');
+      
+    })
+    
+})
